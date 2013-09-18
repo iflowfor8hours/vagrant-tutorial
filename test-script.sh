7370f0f2ab95900d7899c4b35c191af28dabcdab
matt urbanski <matt@iflowfor8hours.info>
2013-09-18 17:38:27 +0200

    major refactoring of tests

--- test-script.sh
+++ test-script.sh
@@ -1,10 +1,35 @@
-# Does the DB file exist? 
-test -f /tmp/flaskr.db
-# Are the permissions correct on the DB file?
-[ "777" = `stat --format=%a /tmp/flaskr.db` ]
-# Are the packages installed
-dpkg --get-selections git | grep git
-dpkg --get-selections python-pip | grep python-pip
-dpkg --get-selections sqlite3 | grep sqlite3
-pip freeze | grep -i flask
+#!/bin/bash
+check_installed () {
+  package=$1
+  dpkg --get-selections $package | grep -q $package
+  installed=$?
+  if [ $installed -ne 0 ]
+  then echo "First install $package" && exit 1
+  fi
+}
 
+check_flask_is_installed () {
+  pip freeze | grep -i flask
+  if [ $? -ne 0 ]
+  then echo "First install Flask (using pip)"
+  fi
+}
+
+check_db_exists () { 
+  if [ ! -f /tmp/flaskr.db ]
+  then echo "Create the database file '/tmp/flaskr.db'" && exit 1
+  fi 
+}
+
+check_db_permissions () {
+  if [  "777" -ne `stat --format=%a /tmp/flaskr.db` ]
+  then echo "Change the permission on the database file '/tmp/flask.db'" && exit 1
+  fi
+}
+
+check_installed git
+check_installed python-pip
+check_installed sqlite3
+check_flask_is_installed
+check_db_exists
+check_db_permissions 
